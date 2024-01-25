1c3a2f7ed90add3c30149118404c1920fbceaf76
TantaiYanhen <124806197+TantaiYanhen@users.noreply.github.com>
2024-01-24 17:42:30 -0800

    Change num from String to Int

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/JokeQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/JokeQueryService.java
@@ -29,17 +29,15 @@ public class JokeQueryService {
 
     public static final String ENDPOINT = "https://v2.jokeapi.dev/joke/{category}?amount={numJokes}";
 
-    public String getJSON(String category, int numJokes) throws HttpClientErrorException {
-        String numJoke = "";
-        numJoke+=numJokes;
-        log.info("category={}, numJokes={}", category, numJoke);
+    public String getJSON(String category, String numJokes) throws HttpClientErrorException {
+        log.info("category={}, numJokes={}", category, numJokes);
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        Map<String, String> uriVariables = Map.of("numJokes", numJoke, "category", category);
+        Map<String, String> uriVariables = Map.of("numJokes", numJokes, "category", category);
 
         ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,
                 uriVariables);
