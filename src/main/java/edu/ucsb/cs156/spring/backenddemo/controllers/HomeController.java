b58010699da16486361479abf003a3dd429db653
Winston Zuo <wzuo@ucsb.edu>
2024-02-01 14:37:52 -0800

    wz - added home controller and modified test

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
@@ -43,12 +43,14 @@ public class HomeController {
         resultMap.put("greeting", "Greetings from Spring Boot!");
 
         List<String> team = new ArrayList<String>();
-        team.add("Jing P.");
-        team.add("Christian S.");
-        team.add("Guy W.");
-        team.add("Phill C.");
+        team.add("Winston Z.");
+        team.add("Christopher W.");
+        team.add("Justin H.");
+        team.add("Sam B.");
+        team.add("Luis B.");
+        team.add("Victor G.");
         resultMap.put("team", team);
-        resultMap.put("repo", "https://github.com/ucsb-cs156-w24/STARTER-team01");
+        resultMap.put("repo", "https://github.com/ucsb-cs156-w24/team01-w24-5pm-1");
         resultMap.put("api-documentation", baseUrl + "swagger-ui/index.html");
         return mapper.writeValueAsString(resultMap);
     }
